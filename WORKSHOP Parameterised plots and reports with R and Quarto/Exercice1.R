b0065cd1f0b94a16ed73beb4faca760f2b083d1e
unknown <rahal.hmza@yahoo.fr>
2024-11-02 22:13:43 +0100

    update exercice1

--- WORKSHOP Parameterised plots and reports with R and Quarto/Exercice1.R
+++ WORKSHOP Parameterised plots and reports with R and Quarto/Exercice1.R
@@ -1,1 +1,35 @@
 ####Exercise 1: Parameterized plots with ggplot2####
+#Load the gapminder data:
+library(gapminder)
+
+#Create a variable to define a year in the gapminder data that you want to plot.
+
+choose_year <- 1952
+
+#use this new year variable
+
+library(dplyr)
+library(ggplot2)
+library(glue)
+
+# Median life expectancy per continent
+gapminder_year <- gapminder |>
+  filter(year == choose_year) |>
+  group_by(continent) |>
+  summarise(
+    median_lifeExp = median(lifeExp)
+  )
+
+# Bar chart
+ggplot(data = gapminder_year) +
+  geom_col(
+    mapping = aes(
+      x = median_lifeExp,
+      y = reorder(continent, median_lifeExp)
+    )
+  ) +
+  labs(
+    x = "Median Life Expectancy (years)", y = NULL,
+    title = glue("Life Expectancy in {choose_year}")
+  )
+
