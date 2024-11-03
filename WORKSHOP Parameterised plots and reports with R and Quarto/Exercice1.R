84b759dd03754270b887747762cf3d52c8fc3f22
unknown <rahal.hmza@yahoo.fr>
2024-11-03 22:18:42 +0100

    Update exercice1

--- WORKSHOP Parameterised plots and reports with R and Quarto/Exercice1.R
+++ WORKSHOP Parameterised plots and reports with R and Quarto/Exercice1.R
@@ -33,3 +33,62 @@ ggplot(data = gapminder_year) +
     title = glue("Life Expectancy in {choose_year}")
   )
 
+###Turn your code into a function that takes the data and year variable as arguments.
+
+barchart_year <- function(choose_year, data = gapminder) {
+  # Median life expectancy per continent
+  gapminder_year <- gapminder |>
+    filter(year == choose_year) |>
+    group_by(continent) |>
+    summarise(
+      median_lifeExp = median(lifeExp)
+    )
+  # Bar chart
+  g <- ggplot(data = gapminder_year) +
+    geom_col(
+      mapping = aes(
+        x = median_lifeExp,
+        y = reorder(continent, median_lifeExp)
+      )
+    ) +
+    labs(
+      x = "Median Life Expectancy (years)", y = NULL,
+      title = glue("Life Expectancy in {choose_year}")
+    )
+  return(g)
+}
+
+#Check it works:
+
+barchart_year(2002)
+
+###Add another argument to your function that allows a user to define the bar colour.
+
+barchart_year <- function(
+    choose_year, fill = "blue", data = gapminder) {
+  # Median life expectancy per continent
+  gapminder_year <- gapminder |>
+    filter(year == choose_year) |>
+    group_by(continent) |>
+    summarise(
+      median_lifeExp = median(lifeExp)
+    )
+  # Bar chart
+  g <- ggplot(data = gapminder_year) +
+    geom_col(
+      mapping = aes(
+        x = median_lifeExp,
+        y = reorder(continent, median_lifeExp)
+      ),
+      fill = fill
+    ) +
+    labs(
+      x = "Median Life Expectancy (years)", y = NULL,
+      title = glue("Life Expectancy in {choose_year}")
+    )
+  return(g)
+}
+
+barchart_year(2002, fill = "purple")
+
+
