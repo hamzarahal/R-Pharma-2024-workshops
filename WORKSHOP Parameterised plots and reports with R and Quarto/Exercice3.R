8085fc7229084f7987dfe245e43afc5d075c5d9d
unknown <rahal.hmza@yahoo.fr>
2024-11-05 22:50:06 +0100

    update

---
+++ WORKSHOP Parameterised plots and reports with R and Quarto/Exercice3.R
@@ -0,0 +1,8 @@
+###############Exercice3: Parameterized text with Quarto
+
+#1. Edit the sentence ...a plot of the median life expectancy in the year 1952 for each continent... 
+#to use inline code to add the document parameter for the year.
+
+#2. Use inline code for the title as well.
+
+#3. Edit the figure caption to use inline code to add the document parameter for the year.
--- WORKSHOP Parameterised plots and reports with R and Quarto/Exercice4.R
+++ WORKSHOP Parameterised plots and reports with R and Quarto/Exercice4.R
@@ -1,1 +1,23 @@
 ###########Exercice4
+
+##1.Render a version of your report for the year 1972 using the quarto_render() function.
+
+quarto_render(
+  input = "report.qmd",
+  execute_params = list(choose_year = 1972)
+)
+
+##2.Render a version of your report for all years in the data.
+
+walk(
+  .x = unique(gapminder$year),
+  .f = ~quarto_render(
+    input = "report.qmd",
+    output_file = glue("report_{.x}.html"),
+    execute_params = list(
+      choose_year = .x
+    )
+  )
+)
+
+
