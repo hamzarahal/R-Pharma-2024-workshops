5e562d5288c9dc93ad14e9d14904f544de6353e3
unknown <rahal.hmza@yahoo.fr>
2024-11-06 22:39:45 +0100

    Update exercice1

--- Unlocking Analysis Results Datasets (ARDs)/Exercice1.R
+++ Unlocking Analysis Results Datasets (ARDs)/Exercice1.R
@@ -65,3 +65,27 @@ ard_stack(
   .overall = TRUE, 
   .total_n = TRUE 
 ) 
+
+# Exercise: Adverse Events summaries using {cards} 
+
+# Load necessary packages 
+library(cards)  
+
+# Import data 
+adsl <- pharmaverseadam::adsl |> dplyr::filter(SAFFL == "Y") 
+adae <- pharmaverseadam::adae  
+
+## A.  Calculate the number and percentage of *unique* subjects (USUBJID) with at least one AE: 
+#  - Overall 
+#  - By each SOC (AESOC) 
+#  - By each Preferred term (AEDECOD) within SOC (AESOC) 
+# By every combination of treatment group (TRT01A) and severity (AESEV) 
+
+ard_stack_hierarchical( 
+  data = adae, 
+  variables = c(AESOC, AEDECOD), 
+  by = c(TRT01A, AESEV),  
+  id = USUBJID, 
+  denominator = adsl, 
+  over_variables = TRUE 
+) 
